ecce26ba8a46ae4c337514b3d25e37c2f42d8d91
kcdt <kcondot@gmail.com>
2025-02-25 15:44:36 +0100

    Feat implémentation de l'authentification (#9)
    * Ajout du model pour l'authentification
    
    * Ajout du controller d'authentification
    
    * Ajout du middleware qui vérifie l'authentification et renommage
    
    * Mise a niveau des routes qui utilisées le middleware de validation suite au renommage
    
    * Ajout d'un schéma de validation pour le login
    
    * Ajout des routes d'authentification
    
    * Ajout du secret pour le refresh token dans le typage du .env et dans le .env.example
    
    * Delete .env

--- packages/backend/src/validation/user.validation.ts
+++ packages/backend/src/validation/user.validation.ts
@@ -7,6 +7,11 @@ export const loginSchema = z.object({
   password: z.string()
 });
 
+export const loginSchema = z.object({
+  email: z.string().email("Email invalide"),
+  password: z.string()
+});
+
 export const createUserSchema = z.object({
   email: z
     .string()
