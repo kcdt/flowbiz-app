0f143d9caf88e246fe81af035e50b91b44436445
Kevin <kcondot@gmail.com>
2025-02-25 11:27:47 +0100

    Mise à jour des schémas users, products et leurs relations

--- packages/backend/src/schemas/relations.schema.ts
+++ packages/backend/src/schemas/relations.schema.ts
@@ -4,3 +4,10 @@ import { products, users } from ".";
 export const usersRelations = relations(users, ({ many }) => ({
   products: many(products),
 }));
+
+export const productsRelations = relations(products, ({ one }) => ({
+  user: one(users, {
+    fields: [products.userId],
+    references: [users.id],
+  }),
+}));
--- packages/backend/src/schemas/users.schema.ts
+++ packages/backend/src/schemas/users.schema.ts
@@ -7,12 +7,12 @@ export const userRoleEnum = pgEnum("user_role", [
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
-  name: varchar('name', { length: 255 }).notNull(),
   email: varchar('email', { length: 255 }).unique().notNull(),
-  status: userRoleEnum().default("seller"),
-  phone: varchar('phone', { length: 255 }).notNull(),
+  name: varchar('name', { length: 255 }).notNull(),
+  role: userRoleEnum('role').default("seller"),
+  phone: varchar('phone', { length: 20 }),
   passwordHash: varchar('password_hash', { length: 255 }).notNull(),
-  refreshToken: varchar('refresh_token', { length: 255 }),
+  companyId: uuid('company_id'), //.references(() => companies.id)
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
