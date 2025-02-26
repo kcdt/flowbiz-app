6bd5e51c90b94bcb54514f48f8c1413d93362d98
Kevin <kcondot@gmail.com>
2025-02-26 14:56:35 +0100

    Implémentation des middleware sur les routes

--- packages/backend/src/routes/product.routes.ts
+++ packages/backend/src/routes/product.routes.ts
@@ -1,23 +1,26 @@
 import { Router } from "express";
 import { productController } from "../controllers/product.controller";
 import { validateRequest } from "../middleware/validation.middleware";
-import { ProductSchema } from "../validation/product.validation";
+import { NewProductSchema, ProductSchema } from "../validation/product.validation";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { checkProductOwner } from "../middleware/owner.middleware";
+import { adminSellerOnly } from "../middleware/role.middleware";
 
 const router = Router();
 
 // [POST] http://localhost:3000/product
-router.post('/', (req, res, next) => validateRequest(ProductSchema, req, res, next), productController.create);
+router.post('/', validateRequest(NewProductSchema), authMiddleware, productController.create);
 
 // [GET] http://localhost:3000/product
-router.get('/', productController.getAll);
+router.get('/', authMiddleware, checkProductOwner, productController.getAll);
 
 // [GET] http://localhost:3000/product/:id
-router.get('/:id', productController.getById);
+router.get('/:id', authMiddleware, checkProductOwner, productController.getById);
 
 // [PATCH] http://localhost:3000/product/:id
-router.patch('/:id', (req, res, next) => validateRequest(ProductSchema, req, res, next), productController.update);
+router.patch('/:id', validateRequest(ProductSchema), authMiddleware, adminSellerOnly, checkProductOwner, productController.update);
 
 // [DELETE] http://localhost:3000/product/:id
-router.delete('/:id', productController.delete);
+router.delete('/:id', authMiddleware, checkProductOwner, adminSellerOnly, productController.delete);
 
 export default router;
--- packages/backend/src/routes/user.routes.ts
+++ packages/backend/src/routes/user.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 import { validateRequest } from "../middleware/validation.middleware";
-import { createUserSchema, updateUserSchema } from "../validation/user.validation";
+import { updateUserSchema } from "../validation/user.validation";
 
 const router = Router();
-
-// [POST] http://localhost:3000/user
-router.post('/', (req, res, next) => validateRequest(createUserSchema, req, res, next), userController.create);
 
 // [GET] http://localhost:3000/user/:id
 router.get('/:id', userController.getById);
@@ -15,7 +12,7 @@ router.get('/:id', userController.getById);
 router.get('/:id/products', userController.getUserProducts);
 
 // [PATCH] http://localhost:3000/user/:id
-router.patch('/:id', (req, res, next) => validateRequest(updateUserSchema, req, res, next), userController.update);
+router.patch('/:id', validateRequest(updateUserSchema), userController.update);
 
 // [DELETE] http://localhost:3000/user/:id
 router.delete('/:id', userController.delete);
