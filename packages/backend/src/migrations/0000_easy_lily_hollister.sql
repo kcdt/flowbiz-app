4e99448427d17098b537a9ff488f6082311d4252
kcdt <kcondot@gmail.com>
2025-02-26 15:01:13 +0100

    Feat amélioration sécurité et vérifications (#11)
    * Renommage de response.utils.ts
    
    * Création du type AuthRequest et amélioration du auth.middleware
    
    * Modification des schemas pour ajouter des verifications (tokens et propriétaire du produit)
    
    * Uniformisation du middleware de validation et suppression du middleware inutilisé
    
    * Retravail des models pour les vérifications
    
    * Implémentation des nouveaux models dans les controllers
    
    * Création de middleware pour vérifier les rôles et les propriétaires des produits
    
    * Ajout d'un schéma de validation pour la création de produit
    
    * Implémentation des middleware sur les routes
    
    * Actualisation des migrations
    
    * Actualisation du script de test de la base et de la fixture

--- packages/backend/src/migrations/0000_easy_lily_hollister.sql
+++ packages/backend/src/migrations/0000_easy_lily_hollister.sql
@@ -1,4 +1,4 @@
-CREATE TYPE "public"."user_role" AS ENUM('seller', 'supplier');--> statement-breakpoint
+CREATE TYPE "public"."user_role" AS ENUM('admin_seller', 'standard_seller', 'supplier');--> statement-breakpoint
 CREATE TABLE "products" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(255) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE "products" (
 	"price" numeric(10, 2) NOT NULL,
 	"quantity" integer DEFAULT 0 NOT NULL,
 	"image_url" varchar(255),
-	"user_id" uuid,
+	"user_id" uuid NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
@@ -15,9 +15,10 @@ CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"email" varchar(255) NOT NULL,
 	"name" varchar(255) NOT NULL,
-	"role" "user_role" DEFAULT 'seller',
+	"role" "user_role" DEFAULT 'standard_seller',
 	"phone" varchar(20),
 	"password_hash" varchar(255) NOT NULL,
+	"refresh_token" varchar(255),
 	"company_id" uuid,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
