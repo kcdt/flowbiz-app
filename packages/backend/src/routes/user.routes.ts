56a3ea3e2bc5ce9a4e89115704a9edcd94447824
Kevin <kcondot@gmail.com>
2025-02-28 16:05:33 +0100

    Ajout des validations manquantes sur les routes users

--- packages/backend/src/routes/user.routes.ts
+++ packages/backend/src/routes/user.routes.ts
@@ -2,16 +2,18 @@ import { Router } from "express";
 import { userController } from "../controllers/user.controller";
 import { validateRequest } from "../middleware/validation.middleware";
 import { updateUserSchema } from "../validation/user.validation";
+import { adminSellerOnly } from "../middleware/role.middleware";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
 // [GET] http://localhost:3000/user/:id
-router.get('/:id', userController.getById);
+router.get('/:id', authMiddleware, adminSellerOnly, userController.getById);
 
 // [PATCH] http://localhost:3000/user/:id
-router.patch('/:id', validateRequest(updateUserSchema), userController.update);
+router.patch('/:id', authMiddleware, validateRequest(updateUserSchema), adminSellerOnly, userController.update);
 
-// [DELETE] http://localhost:3000/user/:id
-router.delete('/:id', userController.delete);
+// [DELETE] http://localhost:3000/user/
+router.delete('/:id', authMiddleware, adminSellerOnly, userController.delete);
 
 export default router;
