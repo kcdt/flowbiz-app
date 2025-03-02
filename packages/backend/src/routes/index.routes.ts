7d389a543d506d9b590691566c15358500126ac8
Kevin <kcondot@gmail.com>
2025-03-02 21:36:59 +0100

    Ajout des routes pour les factures

--- packages/backend/src/routes/index.routes.ts
+++ packages/backend/src/routes/index.routes.ts
@@ -3,20 +3,34 @@ import productRoutes from './product.routes';
 import userRoutes from './user.routes';
 import authRoutes from './auth.routes';
 import saleRoutes from './sale.routes';
+import invoiceRoutes from './invoices.routes';
 
 const router = Router();
 
 // http://localhost:3000/auth
-router.use('/auth', authRoutes);
+router.use('/auth', 
+  authRoutes
+);
 
 // http://localhost:3000/user
-router.use('/user', userRoutes);
+router.use('/user', 
+  userRoutes
+);
 
 // http://localhost:3000/product
-router.use('/product', productRoutes);
+router.use('/product', 
+  productRoutes
+);
 
 // http://localhost:3000/sale
-router.use('/sale', saleRoutes);
+router.use('/sale', 
+  saleRoutes
+);
+
+// http://localhost:3000/invoice
+router.use('/invoice', 
+  invoiceRoutes
+);
 
 router.get('/', (req: Request, res: Response) => {
   res.send(`DB connectée`);
