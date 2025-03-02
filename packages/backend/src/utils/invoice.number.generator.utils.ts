5720104e2fa3b0b5905ce0ae8695100e4fbdfd9a
Kevin <kcondot@gmail.com>
2025-03-02 21:32:38 +0100

    Création de fonction utils pour générer un numéro de facture et fichier un pdf

--- packages/backend/src/utils/invoice.number.generator.utils.ts
+++ packages/backend/src/utils/invoice.number.generator.utils.ts
@@ -5,7 +5,8 @@ import { sql } from 'drizzle-orm';
 export const generateInvoiceNumber = async () => {
   // Récupérer le dernier numéro de facture de l'année en cours
   const currentYear = new Date().getFullYear();
-  const prefix = `INV-${currentYear}-`;
+  const currentMonth = (new Date().getMonth() + 1).toString().padStart(2, '0');
+  const prefix = `INV-${currentYear}-${currentMonth}-`;
   
   const result = await db.select({
     maxNumber: sql`MAX(CAST(SUBSTRING(invoice_number FROM LENGTH(${prefix}) + 1) AS INTEGER))`
