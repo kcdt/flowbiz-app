a4377bbca67bd0faee0b60c80fd6a5d4795664e5
Kevin <kcondot@gmail.com>
2025-03-08 09:30:07 +0100

    Ajout des routes catégories

--- packages/backend/src/routes/index.routes.ts
+++ packages/backend/src/routes/index.routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import productRoutes from './product.routes';
+import productCategoriesRoutes from './product.categories.routes';
 import userRoutes from './user.routes';
 import authRoutes from './auth.routes';
 import saleRoutes from './sale.routes';
@@ -8,27 +9,32 @@ import invoiceRoutes from './invoices.routes';
 const router = Router();
 
 // http://localhost:3000/auth
-router.use('/api/auth', 
+router.use('/auth', 
   authRoutes
 );
 
 // http://localhost:3000/user
-router.use('/api/user', 
+router.use('/user', 
   userRoutes
 );
 
-// http://localhost:3000/product
-router.use('/api/product', 
+// http://localhost:3000/products
+router.use('/products', 
   productRoutes
 );
 
+// http://localhost:3000/product
+router.use('/product-categories', 
+  productCategoriesRoutes
+);
+
 // http://localhost:3000/sale
-router.use('/api/sale', 
+router.use('/sales', 
   saleRoutes
 );
 
 // http://localhost:3000/invoice
-router.use('/api/invoice', 
+router.use('/invoice', 
   invoiceRoutes
 );
 
--- packages/backend/src/routes/product.routes.ts
+++ packages/backend/src/routes/product.routes.ts
@@ -1,41 +1,43 @@
 import { Router } from "express";
 import { productController } from "../controllers/product.controller";
 import { validateRequest } from "../middleware/validation.middleware";
-import { NewProductSchema, ProductSchema } from "../validation/product.validation";
+import { NewProductSchema, EditProductSchema } from "../validation/product.validation";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { checkProductOwner } from "../middleware/owner.middleware";
 import { adminSellerOnly } from "../middleware/role.middleware";
 
 const router = Router();
 
-// [POST] http://localhost:3000/product
+// [POST] http://localhost:3000/products
 router.post('/', 
   validateRequest(NewProductSchema), 
 authMiddleware, 
 productController.create
 );
 
-// [GET] http://localhost:3000/product
+// [GET] http://localhost:3000/products
 router.get('/', 
   authMiddleware, 
   productController.getAll
 );
 
-// [GET] http://localhost:3000/product/:id
+// [GET] http://localhost:3000/products/:id
 router.get('/:id', 
   authMiddleware, 
   checkProductOwner, 
   productController.getById
 );
 
-// [PATCH] http://localhost:3000/product/:id
-router.patch('/:id', 
-  validateRequest(ProductSchema), 
-authMiddleware, 
-adminSellerOnly, checkProductOwner, productController.update
+// [PUT] http://localhost:3000/products/:id
+router.put('/:id',
+  validateRequest(EditProductSchema), 
+  authMiddleware, 
+  adminSellerOnly, 
+  checkProductOwner, 
+  productController.update
 );
 
-// [DELETE] http://localhost:3000/product/:id
+// [DELETE] http://localhost:3000/products/:id
 router.delete('/:id', 
   authMiddleware, 
   adminSellerOnly, 
