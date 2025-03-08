35344d2901c41f744ff8abba774ce08e2daa2d81
Kevin <kcondot@gmail.com>
2025-03-08 22:42:34 +0100

    Ajustements du style global

--- packages/frontend/src/components/sales/SaleListItem.vue
+++ packages/frontend/src/components/sales/SaleListItem.vue
@@ -17,20 +17,18 @@ const openSaleDetails = () => {
 </script>
 
 <template>
-  <div class="sale-item">
-    <div class="sale-infos" v-on:click="openSaleDetails">
-      <h3 class="main-info">{{ props.sale.buyerName }}</h3>
-      <div class="sale-details">
-        <div class="inner-details">
-          <div class="md-col" :class="props.sale.status">
-            <h4>{{ saleStore.statusLabels[props.sale.status] }}</h4>
-          </div>
-          <h4 class="md-col">{{ getFormatedDate(props.sale.date) }}</h4>
+  <div class="sale-infos" v-on:click="openSaleDetails">
+    <h3 class="main-info">{{ props.sale.buyerName }}</h3>
+    <div class="sale-details">
+      <div class="inner-details">
+        <div class="md-col" :class="props.sale.status">
+          <h4>{{ saleStore.statusLabels[props.sale.status] }}</h4>
         </div>
-        <div class="inner-details">
-          <h4 class="xl-col">{{ props.sale.buyerAddress }}</h4>
-          <h3 class="sm-col highligth-info">{{ props.sale.price }} €</h3>
-        </div>
+        <h4 class="md-col">{{ getFormatedDate(props.sale.date) }}</h4>
+      </div>
+      <div class="inner-details">
+        <h4 class="xl-col">{{ props.sale.buyerAddress }}</h4>
+        <h3 class="sm-col highligth-info">{{ props.sale.price }} €</h3>
       </div>
     </div>
   </div>
