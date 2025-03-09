ddb4e54b96fe4643e05b1f2de6a8ce3fdc3ba2ee
Kevin <kcondot@gmail.com>
2025-03-09 17:22:54 +0100

    Ajout d'une méthode pour vérifier si un user peut accéder à une company

--- packages/backend/src/middleware/company.middleware.ts
+++ packages/backend/src/middleware/company.middleware.ts
@@ -27,3 +27,28 @@ export const checkUserCompany = async (req: Request, res: Response, next: NextFu
     return APIResponse(res, null, "Erreur lors de la vérification de l'entreprise", 500);
   }
 };
+
+export const checkCanAccessCompany = async (req: Request, res: Response, next: NextFunction) => {
+  const authReq = req as VerifiedAuthRequest;
+  const userId = authReq.user.id;
+  const companyId = req.params.id;
+  
+  if (!companyId) {
+    return APIResponse(res, null, "ID de l'entreprise non fourni", 400);
+  }
+
+  try {
+    const searchedUser = await userModel.getCompany(userId);
+    if (!searchedUser) {
+      return APIResponse(res, null, "La récupération de l'utilisateur a échoué", 403);
+    }
+
+    if (searchedUser.companyId !== companyId) {
+      return APIResponse(res, null, "Vous ne pouvez pas accéder aux données de cet utilisateur", 500);
+    }
+
+    next();
+  } catch (error) {
+    return APIResponse(res, null, "Erreur lors de la vérification de l'entreprise", 500);
+  }
+};
